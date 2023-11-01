7dc8a17b0f46fcb4f50b64c074121c8d720e7e4c
sjcowdery96 <sjc.cowdery@gmail.com>
2023-10-31 21:37:00 -0500

    create and new routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -16,16 +16,43 @@ breads.get('/', (req, res) => {
     // res.send(Bread)
 })
 
+// CREATE
+breads.post('/', (req, res) => {
+    //added check functionality for inputs
+    //if no image provided, give a placeholder one
+    if (!req.body.image) {
+        req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    }
+    //if input is not a valid url, assign the placeholder
+    if (!req.body.image.startsWith("http") || !req.body.image.startsWith("https")) {
+        req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    }
+
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread.push(req.body)
+    res.redirect('/breads')
+})
+
+// NEW
+breads.get('/new', (req, res) => {
+    res.render('new')
+})
+
 
 // SHOW
+//for moments when we are NOT on the homepage...render SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
         //takes our in-browser parameter and renders the bread we asked for! (remember, it indexes at 0!)
         res.render('Show', {
-            //now we are actully RENDERING the array values
+            //now we are actully RENDERING the array values using our "show" view
             bread: Bread[req.params.arrayIndex]
         })
-        //else throw a 404 error
+        //else throw a 404 error...
     } else {
         res.send('404')
     }
@@ -34,3 +61,9 @@ breads.get('/:arrayIndex', (req, res) => {
 
 
 module.exports = breads
+
+
+/*
+
+
+*/
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.urlencoded({ extended: true }))
+
 
 
 // ROUTES
